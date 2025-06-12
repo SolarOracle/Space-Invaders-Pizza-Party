97f621a17db0156a033e14eaa4f64a3cdf322e2f
Antonio Masetti <antoniopsmasetti@gmail.com>
2025-06-12 15:15:44 -0300

    Added Blocks

--- Scripts/beam.gd
+++ Scripts/beam.gd
@@ -12,6 +12,9 @@ func _process(delta):
 	
 	if (collision):
 		var collider = collision.get_collider()
+		if collider.collision_layer != 8:
+			print(str(collider.collision_layer))
+			collider.hit()
 		hit()
 
 func hit():
---
+++ Scripts/block.gd
@@ -0,0 +1,26 @@
+extends StaticBody2D
+
+@export var max_health : int = 10
+
+var current_health: int = max_health
+
+func hit():
+	current_health = current_health - 1
+	if current_health < 0:
+		current_health = 0
+	if current_health == 0:
+		destroy()
+	change_color()
+	
+func destroy():
+	queue_free()
+
+func change_color():
+	if current_health < 9 and current_health > 6:
+		modulate = Color(0.678431, 1, 0.184314, 1)
+	elif current_health < 7 and current_health > 4:
+		modulate = Color(1, 1, 0, 1)
+	elif current_health < 5 and current_health > 2:
+		modulate = Color(1, 0.270588, 0, 1)
+	elif current_health < 3 and current_health > 0:
+		modulate = Color(1, 0, 0, 1)
