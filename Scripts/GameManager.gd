3f67d4b5d0d79c3acc4d4636004cb72842866ad4
SolarOracle <antoniopsmasetti@gmail.com>
2025-08-20 11:15:10 -0300

    Lives in UI now update

--- Scripts/GameManager.gd
+++ Scripts/GameManager.gd
@@ -1,13 +1,17 @@
 extends Node
 
+@export var lives: int
+
 @onready var player = get_node("/root/TestScene/PlayerTest")
 @onready var invader_manager = $"../InvaderManager"
 @onready var points_label = %PointsLabel
+@onready var lives_label = %LivesNumberLabel
 
 var total_score: int
 
 func _ready():
 	player.death.connect(_on_player_death)
+	lives_label.text = ("%s" % lives)
 
 func lose_game():
 	pass
@@ -19,11 +23,13 @@ func update_score(score):
 	total_score += score
 	if total_score < 100:
 		points_label.text = ("00" + "%s" % total_score)
-	elif total_score > 99:
+	elif total_score < 1000:
 		points_label.text = ("0" + "%s" % total_score)
-	elif total_score > 999:
+	else:
 		points_label.text = ("%s" % total_score)
 
 func _on_player_death():
 	invader_manager.shot_timer.paused = true
-	lose_game()
+	lives -= 1
+	if lives == 0:
+		lose_game()
